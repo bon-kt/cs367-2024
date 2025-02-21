cb3263acd82e4b5474d530c8c4a76f8103574964
bonkt <bonkt11@gmail.com>
2025-02-21 17:27:43 +0700

    HATEAOS code demo (server side)

--- src/main/java/dev/kritchalach/handytools/HandytoolController.java
+++ src/main/java/dev/kritchalach/handytools/HandytoolController.java
@@ -2,9 +2,12 @@ package dev.kritchalach.handytools;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PutMapping;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class HandytoolController {
@@ -22,19 +26,40 @@ public class HandytoolController {
         this.repository = repository;
     }
 
+    // @GetMapping("/handytools")    
+    // List<Storage> findAll() {
+    //     return repository.findAll();
+    // }
     @GetMapping("/handytools")    
-    List<Storage> findAll() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Storage>> findAll() {
+        List<EntityModel<Storage>> storages = repository.findAll().stream()
+            .map(storage -> EntityModel.of(storage,
+                linkTo(methodOn(HandytoolController.class).findOne(storage.getId())).withSelfRel(),
+                linkTo(methodOn(HandytoolController.class).findAll()).withRel("handytools")))
+            .collect(Collectors.toList());
+        return CollectionModel.of(storages, linkTo(methodOn(HandytoolController.class).findAll()).withSelfRel());
     }
 
     @GetMapping("/handytools/{id}")
-    Storage findOne(@PathVariable Long id) {
-        Optional<Storage> handytool =  repository.findById(id);
-        if (handytool.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "no handy tool by given id");
-        }
-        return handytool.get();
+    // EntityModel<Map<String, Storage>> findOne(@PathVariable Long id) {
+    EntityModel<Storage> findOne(@PathVariable Long id) {
+        Storage storage = repository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "no handy tool by given id"));
+        // Map<String, Storage> handytool = Map.of("handytool", storage);
+        // return EntityModel.of( handytool,
+        return EntityModel.of( storage,
+            linkTo(methodOn(HandytoolController.class).findOne(id)).withSelfRel(),
+            linkTo(methodOn(HandytoolController.class).findAll()).withRel("handytools"));
     }
+    
+    // @GetMapping("/handytools/{id}")
+    // Storage findOne(@PathVariable Long id) {
+    //     Optional<Storage> handytool =  repository.findById(id);
+    //     if (handytool.isEmpty()) {
+    //         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "no handy tool by given id");
+    //     }
+    //     return handytool.get();
+    // }
     
     @PostMapping("/handytools")
     Storage newHandyTool(@RequestBody Storage handytool) {        
